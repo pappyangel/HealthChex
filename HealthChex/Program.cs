23eba84d822a035c35d2bb7f9c08f83ef8b8ca24
Jim Vacca <33520581+MSFTJim@users.noreply.github.com>
2024-05-19 16:57:38 +0000

    chore: Add health checks and UI packages

--- HealthChex/Program.cs
+++ HealthChex/Program.cs
@@ -1,3 +1,7 @@
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,7 +9,27 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddHealthChecks();
+builder.Services.AddHealthChecks()        
+    .AddCheck<CustomHealthCheck>("CustomHealthCheck", tags: new[] { "custom" })
+    .AddCheck<DBHealthCheck>("DBHealthCheck", tags: new[] { "DB" });
+
+builder.Services.AddHealthChecksUI().AddInMemoryStorage();
+
 var app = builder.Build();
+
+app.MapHealthChecksUI();
+
+app.MapHealthChecks("/healthz");
+app.MapHealthChecks("/health", new HealthCheckOptions
+{
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+});
+app.MapHealthChecks("/health/custom", new HealthCheckOptions
+{
+    Predicate = reg => reg.Tags.Contains("custom"),
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+});
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -41,4 +65,43 @@ app.Run();
 record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 {
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
+public class CustomHealthCheck : IHealthCheck
+{
+    public Task<HealthCheckResult> CheckHealthAsync(
+        HealthCheckContext context, CancellationToken cancellationToken = default)
+    {
+        Random _random = new Random();
+        var responseTime = _random.Next(1, 300);
+        if (responseTime < 100)
+        {
+            return Task.FromResult(HealthCheckResult.Healthy("Healthy result from MyHealthCheck"));
+        }
+        else if (responseTime < 200)
+        {
+            return Task.FromResult(HealthCheckResult.Degraded("Degraded result from MyHealthCheck"));
+        }
+
+        return Task.FromResult(HealthCheckResult.Unhealthy("Unhealthy result from MyHealthCheck"));
+    }
+}
+
+public class DBHealthCheck : IHealthCheck
+{
+    public Task<HealthCheckResult> CheckHealthAsync(
+        HealthCheckContext context, CancellationToken cancellationToken = default)
+    {
+        Random _random = new Random();
+        var responseTime = _random.Next(1, 300);
+        if (responseTime < 100)
+        {
+            return Task.FromResult(HealthCheckResult.Healthy("Healthy result from MyHealthCheck"));
+        }
+        else if (responseTime < 200)
+        {
+            return Task.FromResult(HealthCheckResult.Degraded("Degraded result from MyHealthCheck"));
+        }
+
+        return Task.FromResult(HealthCheckResult.Unhealthy("Unhealthy result from MyHealthCheck"));
+    }
 }
